0541cc590e6713a26555535c879f5416b04ff4df
Quentin Perthuis <perthuis.quentin@gmail.com>
2025-06-22 21:27:13 +0200

    :construction: Test open graph properties

--- src/app/agents/[agentId]/page.tsx
+++ src/app/agents/[agentId]/page.tsx
@@ -1,9 +1,32 @@
+import { Metadata } from 'next';
+
 import Agent from '@/components/pages/agent.page';
 import { AgentId } from '@/data/agent.type';
+import { agents, getAgentIconUrl } from '@/data/agents.data';
 
 type AgentPageProps = {
     params: Promise<{ agentId: AgentId }>
 };
+
+export async function generateMetadata({ params }: AgentPageProps): Promise<Metadata> {
+    const { agentId } = await params;
+    const agent = agents.getById(agentId)!;
+
+    return {
+        openGraph: {
+            title: agent.name,
+            description: agent.name,
+            url: `/agents/${agentId}`,
+            images: [
+                {
+                    url: getAgentIconUrl(agentId),
+                    width: 128,
+                    height: 128,
+                },
+            ],
+        }
+    };
+}
 
 export default async function AgentPage({ params }: AgentPageProps) {
     const { agentId } = await params;
