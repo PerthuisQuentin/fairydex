2adf41638ce32bac319f995226c72754649c9671
Quentin Perthuis <perthuis.quentin@gmail.com>
2025-06-23 00:09:09 +0200

    :lipstick: Design for bangboo list

--- src/components/pages/bangboos.page.tsx
+++ src/components/pages/bangboos.page.tsx
@@ -1,38 +1,19 @@
-import Link from 'next/link';
-
 import { bangboos } from '@/data/bangboos.data';
-import { Bangboo } from '@/models/bangboo/bangboo.model';
 
-import BangbooIcon from '../bangboo/bangboo-icon';
-import BangbooRankIcon from '../rank/bangboo-rank-icon';
-
-const getBangbooCard = (bangboo: Bangboo) => {
-    return (
-        <Link
-            key={bangboo.id}
-            href={`/bangboos/${bangboo.id}`}
-            className="flex items-center p-4 m-4 border border-gray-200 transition-transform duration-150 hover:scale-105 hover:shadow-lg"
-            style={{ textDecoration: 'none', color: 'inherit' }}
-        >
-            <div className='h-32 w-32 relative'>
-                <BangbooIcon bangbooId={bangboo.id} />
-            </div>
-            <div className='ml-6'>
-                <h2 className="text-xl font-bold">{bangboo.name}</h2>
-                <div className="flex flex-row gap-2 items-center text-base">
-                    <div className='h-8 w-8 relative'>
-                        <BangbooRankIcon rank={bangboo.rank} />
-                    </div>
-                </div>
-            </div>
-        </Link>
-    );
-};
+import BangbooCard from '../bangboo/bangboo-card';
 
 export default function Bangboos() {
     return (
-        <div className="mx-auto max-w-4xl">
-            {bangboos.getAll().map((bangboo) => getBangbooCard(bangboo))}
+        <div className="w-full flex justify-center">
+            <div className="w-full max-w-6xl px-4">
+                <div className="grid justify-center [grid-template-columns:repeat(auto-fit,minmax(180px,max-content))]">
+                    {bangboos.getAll().map((bangboo) => (
+                        <div className="flex justify-center items-center" key={bangboo.id}>
+                            <BangbooCard bangboo={bangboo} />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
