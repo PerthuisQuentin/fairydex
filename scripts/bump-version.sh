63fc805e8af21c1877cc42e1f900a205d1c2eee1
Quentin Perthuis <perthuis.quentin@gmail.com>
2025-08-11 20:09:04 +0200

    :sparkles: Add release script

---
+++ scripts/bump-version.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+set -e
+
+if [[ -z "$1" ]]; then
+  echo -n 'Enter new version: '
+  read VERSION
+  if [[ -z "$VERSION" ]]; then
+    echo "No version provided. Aborting."
+    exit 1
+  fi
+else
+  VERSION="$1"
+fi
+
+sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" package.json
+git add package.json
+git commit -m ":bookmark: $VERSION"
+git tag v$VERSION
+
